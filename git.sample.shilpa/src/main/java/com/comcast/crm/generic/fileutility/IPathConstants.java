fcd244a1c3a25aa8ec03d0fbb1d6f7e7575ca3c0
suraj <suraj@suraj-pavilion>
2025-02-24 10:51:19 +0530

    updated on Ipath

--- git.sample.shilpa/src/main/java/com/comcast/crm/generic/fileutility/IPathConstants.java
+++ git.sample.shilpa/src/main/java/com/comcast/crm/generic/fileutility/IPathConstants.java
@@ -3,5 +3,5 @@ package com.comcast.crm.generic.fileutility;
 public interface IPathConstants {
 
 	
-	String excel = ".src/testData/commonData.xls";
+	String excelPath = ".src/testData/commonData.xls";
 }
