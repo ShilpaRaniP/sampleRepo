2f7d886f35d75043dce65fa5e2042f1edd5c90d1
DELL <DELL@DESKTOP-23IQLTI.mshome.net>
2025-02-24 10:42:45 +0530

    $$$$$$

--- git.sample.shilpa/src/main/java/com/comcast/crm/basetest/BaseClass.java
+++ git.sample.shilpa/src/main/java/com/comcast/crm/basetest/BaseClass.java
@@ -95,7 +95,7 @@ public class BaseClass {
 	{
 		System.out.println("=====close the BROWSER=====");
 		driver.quit();
-		System.out.println("After class updated =====$$$$");
+		System.out.println("After class updated =====$$$");
 	}
 	
 	@AfterSuite(alwaysRun = true)
